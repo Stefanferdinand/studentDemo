e8cec09fe9d1e54128e5c203be487a1b8bd6259d
STEFAN FERDINAND <stefan.ferdinand75@gmail.com>
2023-07-08 00:40:14 +0700

    add get one student

--- src/main/java/com/example/studentDemo/Student/StudentController.java
+++ src/main/java/com/example/studentDemo/Student/StudentController.java
@@ -2,6 +2,7 @@ package com.example.studentDemo.Student;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class StudentController {
     @GetMapping
     public ResponseEntity<?> getAll(){
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(@PathVariable long id){
+        return ResponseEntity.ok(repository.findById(id).orElseThrow(() -> new RuntimeException()));
     }
 }
